4df6e5053935b40f7821a2a46179db7b674ac137
joannaberlin <joanna.kot.ok@gmail.com>
2024-03-14 15:29:31 +0100

    Add styles for board and buttons

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const newDeckBtn = document.getElementById('new-deck');
 const drawBtn = document.getElementById('draw');
 const cardsWrapper = document.getElementById('cards_wrapper');
 let deckId;
-//https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2
+
 const handleNewDeckClick = () => {
 	fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
 		.then((response) => response.json())
 		.then((data) => {
-			console.log(data);
 			deckId = data.deck_id;
-			console.log(deckId);
 		});
 };
 
@@ -17,13 +15,10 @@ const handleDrawClick = () => {
 	fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
 		.then((res) => res.json())
 		.then((data) => {
-			console.log(data);
 			const cardsArr = data.cards;
-			console.log(cardsArr);
 
 			for (let card of cardsArr) {
 				const imgUrl = card.image;
-				console.log(imgUrl);
 				cardsWrapper.innerHTML += `<img src="${imgUrl}"/>`;
 			}
 		});
