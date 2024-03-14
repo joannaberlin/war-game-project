9ff97d9b177bd8e5e3fac77f1be9873466720922
joannaberlin <joanna.kot.ok@gmail.com>
2024-03-14 14:05:36 +0100

    Add button to fetch another data, fetch the data on click

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', () => {
+const newDeckBtn = document.getElementById('new-deck');
+const drawBtn = document.getElementById('draw');
+let deckId;
+//https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2
+const handleNewDeckClick = () => {
 	fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
 		.then((response) => response.json())
-		.then((data) => console.log(data));
-});
+		.then((data) => {
+			console.log(data);
+			deckId = data.deck_id;
+			console.log(deckId);
+		});
+};
+
+const handleDrawClick = () => {
+	fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+		.then((res) => res.json())
+		.then((data) => console.log(data.cards));
+};
+
+newDeckBtn.addEventListener('click', handleNewDeckClick);
+drawBtn.addEventListener('click', handleDrawClick);
