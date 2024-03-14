5533bde95f1750ab1dab5fb3fc21155f963df855
joannaberlin <joanna.kot.ok@gmail.com>
2024-03-14 14:31:50 +0100

    Render cards images on the page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const newDeckBtn = document.getElementById('new-deck');
 const drawBtn = document.getElementById('draw');
+const cardsWrapper = document.getElementById('cards_wrapper');
 let deckId;
 //https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2
 const handleNewDeckClick = () => {
@@ -15,7 +16,18 @@ const handleNewDeckClick = () => {
 const handleDrawClick = () => {
 	fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
 		.then((res) => res.json())
-		.then((data) => console.log(data.cards));
+		.then((data) => {
+			console.log(data);
+			const cardsArr = data.cards;
+			console.log(cardsArr);
+
+			for (let card of cardsArr) {
+				const imgUrl = card.image;
+				console.log(imgUrl);
+				cardsWrapper.innerHTML += `<img src="${imgUrl}"/>`;
+			}
+		});
+	cardsWrapper.innerHTML = '';
 };
 
 newDeckBtn.addEventListener('click', handleNewDeckClick);
