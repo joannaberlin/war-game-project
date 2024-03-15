cba7db13bedade6617a180c54810f94cc0f4355d
joannaberlin <joanna.kot.ok@gmail.com>
2024-03-15 14:18:38 +0100

    Add algorithm to determine winner

--- index.js
+++ index.js
@@ -11,6 +11,34 @@ const handleNewDeckClick = () => {
 		});
 };
 
+const determineCardWinner = (card1, card2) => {
+	const values = [
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9',
+		'10',
+		'JACK',
+		'QUEEN',
+		'KING',
+		'ACE',
+	];
+	const card1ValueIndex = values.indexOf(card1.value);
+	const card2ValueIndex = values.indexOf(card2.value);
+
+	if (card1ValueIndex > card2ValueIndex) {
+		console.log('Card1 wins!');
+	} else if (card1ValueIndex < card2ValueIndex) {
+		console.log('Card2 wins!');
+	} else if (card1ValueIndex === card2ValueIndex) {
+		console.log(`It's a tie!`);
+	}
+};
+
 const handleDrawClick = () => {
 	fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
 		.then((res) => res.json())
@@ -21,6 +49,10 @@ const handleDrawClick = () => {
 			cardsWrapper.children[1].innerHTML = `
 				<img src=${data.cards[1].image} class="card" />
 			`;
+
+			console.log(data.cards);
+
+			determineCardWinner(data.cards[0], data.cards[1]);
 		});
 };
 
